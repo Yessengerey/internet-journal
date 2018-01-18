286c35378f72b4c9f928da03af8d56b021893240
Gary <yes.murz@gmail.com>
2018-01-18 08:23:01 +0600

    Add opacity change on click to divs

--- client/src/components/landing/index.jsx
+++ client/src/components/landing/index.jsx
@@ -16,77 +16,97 @@ export default class Landing extends Component {
       panel: 0
     }
 
-    this.teacherPanelButtonClickHandler = this.teacherPanelButtonClickHandler.bind(this);
-    this.studentPanelButtonClickHandler = this.studentPanelButtonClickHandler.bind(this);
-    this.contactUsPanelButtonClickHandler = this.contactUsPanelButtonClickHandler.bind(this);
-
+    this.panelButtonHandler = this.panelButtonHandler.bind(this);
   }
 
-  teacherPanelButtonClickHandler() {
-    // if ( document.getElementById(style.inkbar_container).classList.contains(style.slideToTeacher) ) {
-      document.getElementById(style.inkbar_container).classList.remove(style.slideToTeacher);
-      document.getElementById(style.inkbar_container).classList.remove(style.slideToStudent);
-      document.getElementById(style.inkbar_container).classList.remove(style.slideToContact);
-      document.getElementById(style.inkbar_container).classList.add(style.slideToTeacher);
-    // } else {
-    //   document.getElementById(style.inkbar_container).classList.add(style.slideToTeacher);
-    // }
+  componentDidMount() {
+    document.getElementById('studentButton').classList.add(style.active_button);
   }
 
-  studentPanelButtonClickHandler() {
-    // if ( document.getElementById(style.inkbar_container).classList.contains(style.slideToStudent) ) {
-      document.getElementById(style.inkbar_container).classList.remove(style.slideToTeacher);
-      document.getElementById(style.inkbar_container).classList.remove(style.slideToStudent);
-      document.getElementById(style.inkbar_container).classList.remove(style.slideToContact);
-      document.getElementById(style.inkbar_container).classList.add(style.slideToStudent);
-    // } else {
-    //   document.getElementById(style.inkbar_container).classList.add(style.slideToStudent);
-    // }
-  }
+  panelButtonHandler(event) {
+    const inkbarElm = document.getElementById(style.inkbar_container);
 
-  contactUsPanelButtonClickHandler() {
-    // if ( document.getElementById(style.inkbar_container).classList.contains(style.slideToContact) ) {
-      document.getElementById(style.inkbar_container).classList.remove(style.slideToTeacher);
-      document.getElementById(style.inkbar_container).classList.remove(style.slideToStudent);
-      document.getElementById(style.inkbar_container).classList.remove(style.slideToContact);
-      document.getElementById(style.inkbar_container).classList.add(style.slideToContact);
-    // } else {
-    //   document.getElementById(style.inkbar_container).classList.add(style.slideToContact);
-    // }
+    const stuElm = document.getElementById('studentButton');
+    const teaElm = document.getElementById('teacherButton');
+    const conElm = document.getElementById('contactUsButton');
+
+    inkbarElm.classList.remove(style.slideToTeacher);
+    inkbarElm.classList.remove(style.slideToStudent);
+    inkbarElm.classList.remove(style.slideToContact);
+
+    if (event.target.id === 'studentButton') {
+      this.setState({
+        panel: 0
+      });
+      teaElm.classList.remove(style.active_button);
+      conElm.classList.remove(style.active_button);
+
+      stuElm.classList.add(style.active_button);
+      inkbarElm.classList.add(style.slideToStudent);
+    } else if (event.target.id === 'teacherButton') {
+      this.setState({
+        panel: 1
+      });
+      stuElm.classList.remove(style.active_button);
+      conElm.classList.remove(style.active_button);
+
+      teaElm.classList.add(style.active_button);
+      inkbarElm.classList.add(style.slideToTeacher);
+    } else if (event.target.id === 'contactUsButton') {
+      this.setState({
+        panel: 2
+      });
+      stuElm.classList.remove(style.active_button);
+      teaElm.classList.remove(style.active_button);
+
+      conElm.classList.add(style.active_button);
+      inkbarElm.classList.add(style.slideToContact);
+    }
   }
 
   render() {
+
     return (
       <div className={style.main_card}>
 
         <div className={style.panels_container}>
+
           <div className={style.logo_container}>
             LOGO
           </div>
+
           <div className={style.panels_outer_container}>
             <div className={style.user_panel}>
               PANEL
             </div>
           </div>
+
           <div className={style.panel_view_controls}>
+
             <div className={style.panel_view_buttons}>
-              <div id={style.student_panel_button}
-                onClick={this.studentPanelButtonClickHandler}>
-                <span>ученикам и родителям</span>
+
+              <div
+                onClick={this.panelButtonHandler}>
+                <span id='studentButton'>ученикам и родителям</span>
               </div>
-              <div id={style.teacher_panel_button}
-                onClick={this.teacherPanelButtonClickHandler}>
-                <span>учебным заведениям</span>
+
+              <div
+                onClick={this.panelButtonHandler}>
+                <span id='teacherButton'>учебным заведениям</span>
               </div>
-              <div id={style.contact_us_panel_button}
-                onClick={this.contactUsPanelButtonClickHandler}>
-                <span>поддержка и обратная связь</span>
+
+              <div
+                onClick={this.panelButtonHandler}>
+                <span id='contactUsButton'>поддержка и обратная связь</span>
               </div>
+
             </div>
+
             <div id={style.inkbar_container}>
               <hr className={style.inkbar}/>
             </div>
           </div>
+
         </div>
 
         <div className={style.dynamic_container}>
---
+++ client/src/components/landing/studentsParentPanel.jsx
@@ -0,0 +1,26 @@
+// React
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+export const StudentPanel = (props) => {
+  return (
+    <div>
+      <div>
+        <div>
+
+        </div>
+      </div>
+      <div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+        <div>
+          
+        </div>
+      </div>
+    </div>
+  )
+}
