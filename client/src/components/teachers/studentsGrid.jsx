1f59da61d4caf02c591c9f5f69d7a27b50da8715
Gary <yes.murz@gmail.com>
2018-01-26 10:00:15 +0600

    MVP messages

--- client/src/components/teachers/studentsGrid.jsx
+++ client/src/components/teachers/studentsGrid.jsx
@@ -11,78 +11,70 @@ import Visibility from 'material-ui/svg-icons/action/visibility';
 import {red500, yellow500, blue500} from 'material-ui/styles/colors';
 
 
-import style from '../../../../styles/teachers/classesGrid.css';
+import style from '../../../../styles/teachers/studentsGrid.css';
 
-export default class ClassesGrid extends Component {
+export default class StudentsGrid extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      classes: []
+      students: []
     }
 
-    this.renderClasses = this.renderClasses.bind(this);
+    this.renderStudents = this.renderStudents.bind(this);
     this.handleRadio = this.handleRadio.bind(this);
   }
 
   handleRadio(e) {
   }
 
-  renderClasses() {
-    var classesToRender = [];
+  renderStudents() {
+    var studentsToRender = [];
 
-    for (var i = 0; i < this.state.classes.length; i+=3) {
+    for (var i = 0; i < this.state.students.length; i+=2) {
       var classItem =
         <MuiThemeProvider key={i}><div className={style.class_table_row}>
           <Checkbox
-            style={{width: '33.33%'}}
-            label={this.state.classes[i]}
-            value={this.state.classes[i]}
+            style={{width: '50%'}}
+            label={this.state.students[i]}
+            value={this.state.students[i]}
             onCheck={this.handleRadio}
             iconStyle={{fill: '#67428F'}}
             labelStyle={{fontSize: '1.1vw'}}
           />
           <Checkbox
-            style={{width: '33.33%'}}
-            label={this.state.classes[i+1]}
-            value={this.state.classes[i+1]}
-            onCheck={this.handleRadio}
-            iconStyle={{fill: '#67428F'}}
-            labelStyle={{fontSize: '1.1vw'}}
-          />
-          <Checkbox
-            style={{width: '33.33%'}}
-            label={this.state.classes[i+2]}
-            value={this.state.classes[i+2]}
+            style={{width: '50%'}}
+            label={this.state.students[i+1]}
+            value={this.state.students[i+1]}
             onCheck={this.handleRadio}
             iconStyle={{fill: '#67428F'}}
             labelStyle={{fontSize: '1.1vw'}}
           />
         </div></MuiThemeProvider>;
 
-      classesToRender.push(classItem);
+      studentsToRender.push(classItem);
     }
 
-    return classesToRender;
+    return studentsToRender;
   }
 
   componentWillMount() {
     this.setState({
-      classes: this.props.classes
+      students: this.props.students
     })
   }
 
   componentWillReceiveProps(nextProps) {
-    if (JSON.stringify(this.state.classes) !== JSON.stringify(nextProps.classes)) {
+    if (JSON.stringify(this.state.students) !== JSON.stringify(nextProps.students)) {
       this.setState({
-        classes: nextProps.classes
+        students: nextProps.students
       })
     }
   }
 
   render() {
     return (
-      <div className={style.classes_main_container}>
-        {this.renderClasses().map((item) => item)}
+      <div className={style.students_main_container}>
+        {this.renderStudents().map((item) => item)}
       </div>
     )
   }
--- client/src/containers/teachers/messages.jsx
+++ client/src/containers/teachers/messages.jsx
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 
 import MessagesMenu from '../../components/utility/messagesMenu.jsx';
 import ClassesGrid from '../../components/teachers/classesGrid.jsx';
+import StudentsGrid from '../../components/teachers/studentsGrid.jsx';
 import Checkbox from 'material-ui/Checkbox';
 
 import DataGrid from '../../components/utility/datagrid.jsx';
 import Dialog from 'material-ui/Dialog';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Switch from '../../components/utility/switch.jsx';
+import DropDownMenu from 'material-ui/DropDownMenu';
+import MenuItem from 'material-ui/MenuItem';
 
 import IconButton from 'material-ui/IconButton';
 import CheckIcon from 'material-ui/svg-icons/navigation/check';
@@ -29,29 +32,29 @@ export default class Messages extends Component {
           {value: 'От кого', readOnly: true, width: 180},
           {value: 'Тема', readOnly: true, width: 500}
         ],
-          [{component: <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}><div style={{height: '10px', width: '10px', backgroundColor: 'red'}}></div></div>, forceComponent: true}, {value: 'Математика 05/01/2018'}, {value: ''}],
-          [{component: <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}><div style={{height: '10px', width: '10px', backgroundColor: 'purple'}}></div></div>, forceComponent: true}, {value: 'Литература 05/01/2018'}, {value: ''}],
-          [{component: <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}><div style={{height: '10px', width: '10px', backgroundColor: 'red'}}></div></div>, forceComponent: true}, {value: 'Биология 05/01/2018'}, {value: ''}],
-          [{component: iconEl, forceComponent: true}, {value: 'Черчение 05/01/2018'}, {value: ''}],
-          [{component: iconEl, forceComponent: true}, {value: 'Английский 05/01/2018'}, {value: ''}],
-          [{component: iconEl, forceComponent: true}, {value: 'Физика 05/01/2018'}, {value: ''}],
-          [{component: iconEl, forceComponent: true}, {value: 'Физкультура 05/01/2018'}, {value: ''}],
-          [{component: iconEl, forceComponent: true}, {value: 'Казахский 05/01/2018'}, {value: ''}],
-          [{component: iconEl, forceComponent: true}, {value: 'Русский 05/01/2018'}, {value: ''}],
-          [{component: iconEl, forceComponent: true}, {value: 'География 05/01/2018'}, {value: ''}],
-          [{component: iconEl, forceComponent: true}, {value: 'Технологии 05/01/2018'}, {value: ''}],
-          [{component: iconEl, forceComponent: true}, {value: 'Искусство 05/01/2018'}, {value: ''}],
-          [{component: iconEl, forceComponent: true}, {value: 'История М. 05/01/2018'}, {value: ''}],
-          [{component: iconEl, forceComponent: true}, {value: 'История К. 05/01/2018'}, {value: ''}],
-          [{component: iconEl, forceComponent: true}, {value: 'Литература К. 05/01/2018'}, {value: ''}],
-          [{component: iconEl, forceComponent: true}, {value: 'Музыка 05/01/2018'}, {value: ''}],
-          [{component: iconEl, forceComponent: true}, {value: 'Химия 05/01/2018'}, {value: ''}],
-          [{component: iconEl, forceComponent: true}, {value: 'Французский 05/01/2018'}, {value: ''}],
-          [{component: iconEl, forceComponent: true}, {value: 'Проф 05/01/2018'}, {value: ''}],
-          [{component: iconEl, forceComponent: true}, {value: 'Проф 05/01/2018'}, {value: ''}]
+        [{component: <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}><div style={{height: '10px', width: '10px', backgroundColor: 'red'}}></div></div>, forceComponent: true}, {value: 'Математика 05/01/2018'}, {value: ''}],
+        [{component: <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}><div style={{height: '10px', width: '10px', backgroundColor: 'purple'}}></div></div>, forceComponent: true}, {value: 'Литература 05/01/2018'}, {value: ''}],
+        [{component: <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}><div style={{height: '10px', width: '10px', backgroundColor: 'red'}}></div></div>, forceComponent: true}, {value: 'Биология 05/01/2018'}, {value: ''}],
+        [{component: iconEl, forceComponent: true}, {value: 'Черчение 05/01/2018'}, {value: ''}],
+        [{component: iconEl, forceComponent: true}, {value: 'Английский 05/01/2018'}, {value: ''}],
+        [{component: iconEl, forceComponent: true}, {value: 'Физика 05/01/2018'}, {value: ''}],
+        [{component: iconEl, forceComponent: true}, {value: 'Физкультура 05/01/2018'}, {value: ''}],
+        [{component: iconEl, forceComponent: true}, {value: 'Казахский 05/01/2018'}, {value: ''}],
+        [{component: iconEl, forceComponent: true}, {value: 'Русский 05/01/2018'}, {value: ''}],
+        [{component: iconEl, forceComponent: true}, {value: 'География 05/01/2018'}, {value: ''}],
+        [{component: iconEl, forceComponent: true}, {value: 'Технологии 05/01/2018'}, {value: ''}],
+        [{component: iconEl, forceComponent: true}, {value: 'Искусство 05/01/2018'}, {value: ''}],
+        [{component: iconEl, forceComponent: true}, {value: 'История М. 05/01/2018'}, {value: ''}],
+        [{component: iconEl, forceComponent: true}, {value: 'История К. 05/01/2018'}, {value: ''}],
+        [{component: iconEl, forceComponent: true}, {value: 'Литература К. 05/01/2018'}, {value: ''}],
+        [{component: iconEl, forceComponent: true}, {value: 'Музыка 05/01/2018'}, {value: ''}],
+        [{component: iconEl, forceComponent: true}, {value: 'Химия 05/01/2018'}, {value: ''}],
+        [{component: iconEl, forceComponent: true}, {value: 'Французский 05/01/2018'}, {value: ''}],
+        [{component: iconEl, forceComponent: true}, {value: 'Проф 05/01/2018'}, {value: ''}],
+        [{component: iconEl, forceComponent: true}, {value: 'Проф 05/01/2018'}, {value: ''}]
       ],
       open: false,
-      currentMailView: '5',
+      currentMailView: '0',
       currentSwitchView: 'По классам',
       classes: [
         '1A', '1B', '1C',
@@ -65,45 +68,115 @@ export default class Messages extends Component {
         '9A', '9B', '9C',
         '10A', '10B', '10C',
         '11A', '11B', '11C'],
+        students: [
+          'Угоров Е. Ж.',
+          'Шамилин У. Ю.',
+          'Каратаева З. Р.',
+          'Устепов К. А.',
+          'Куропаткина Ж. У.',
+          'Фандоринов К. К.',
+          'Щянтарин Г. К.',
+          'Сунтарова Ф. А.',
+          'Руссов С. С.',
+          'Самилинова А. В.',
+          'Акматов А. А.',
+          'Симворов А. С.',
+          'Ломоносова М. А.',
+          'Ломоносов К. А.',
+          'Болат А. А.',
+          'Мырза Ж. Б.',
+          'Химтар Ж. Ж.',
+          'Йоркина С. В.',
+          'Сарыбек А. Ч.',
+          'Чехов А. С.',
+        ],
+        classFocusValue: 0
+      }
+
+      this.handleOpenDialog = this.handleOpenDialog.bind(this);
+      this.handleCloseDialog = this.handleCloseDialog.bind(this);
+      this.changeMailView = this.changeMailView.bind(this);
+      this.switchView = this.switchView.bind(this);
+      this.handleDropDown = this.handleDropDown.bind(this);
     }
 
-    this.handleOpenDialog = this.handleOpenDialog.bind(this);
-    this.handleCloseDialog = this.handleCloseDialog.bind(this);
-    this.changeMailView = this.changeMailView.bind(this);
-    this.switchView = this.switchView.bind(this);
-  }
+    handleDropDown(e, index, value) {
+      this.setState({
+        classFocusValue: value
+      })
+    }
 
-  switchView(nextView) {
-    this.setState({
-      currentSwitchView: nextView
-    })
-  }
+    switchView(nextView) {
+      this.setState({
+        currentSwitchView: nextView
+      })
+    }
 
-  changeMailView(nextView) {
-    this.setState({
-      currentMailView: nextView
-    })
-  }
+    changeMailView(nextView) {
+      this.setState({
+        currentMailView: nextView
+      })
+    }
 
-  handleOpenDialog() {
-    this.setState({
-      open: true
-    })
-  }
+    handleOpenDialog() {
+      this.setState({
+        open: true
+      })
+    }
 
-  handleCloseDialog() {
-    this.setState({
-      open: false
-    })
-  }
+    handleCloseDialog() {
+      this.setState({
+        open: false
+      })
+    }
 
 
-  render() {
+    render() {
 
-    let mailView = <div></div>;
+      let studentsGridEl = <StudentsGrid students={this.state.students}/>;
+      let classesGridEl = <ClassesGrid classes={this.state.classes} />;
+      let leftGridTitle = '';
+      let classesDropDownEl = <div></div>;
+      let bottomElement = <div></div>;
 
-    if (this.state.currentMailView === '0') {
-      mailView =  <div className={style.inbox}>
+      let gridElement = <div></div>;
+
+      if (this.state.currentSwitchView === 'По классам') {
+        leftGridTitle = 'Выберите класс/Несколько классов';
+        gridElement = classesGridEl;
+        classesDropDownEl = <div></div>;
+        bottomElement = <div className={style.choose}>
+          Выберите дату
+          <div className={style.dd_mm}>
+            ДД/ММ
+          </div>
+          <Checkbox
+            style={{width: '185px'}}
+            label={'Сегодняшнее число'}
+            value={'Сегодняшнее число'}
+            iconStyle={{fill: '#67428F'}}
+            labelStyle={{fontSize: '10px'}}
+          />
+
+        </div>
+      } else if (this.state.currentSwitchView === 'По ученикам') {
+        leftGridTitle = 'Выберите ученика';
+        gridElement = studentsGridEl;
+        classesDropDownEl =
+        <div className={style.dropdown_container}>
+          <span className={style.dropdown_title}>Выберите класс</span>
+          <DropDownMenu maxHeight={300} value={this.state.classFocusValue} onChange={this.handleDropDown}>
+            {this.state.classes.map((item, id) => (<MenuItem value={id} key={id} primaryText={item} />))}
+          </DropDownMenu>
+        </div>;
+
+        bottomElement = <div className={style.choose}></div>;
+      }
+
+      let mailView = <div></div>;
+
+      if (this.state.currentMailView === '0') {
+        mailView =  <div className={style.inbox}>
           <div className={style.header_container}>
             <div className={style.button_container} onClick={this.handleOpenDialog}>
               <div className={style.button}>Написать</div>
@@ -118,142 +191,128 @@ export default class Messages extends Component {
             numberedRows={false}
             width={'100%'}
             data={this.state.data}/>
-        </div>;
-    } else if (this.state.currentMailView === '5') {
-      mailView =  <div style={{width: '80%'}}>
-      <div className={style.classes_table_outer_container}>
-          {/* <div className={style.class_student_switch}> */}
-            <Switch
-            leftTitle={'По классам'}
-            rightTitle={'По ученикам'}
-            switchView={this.switchView}
-            width='60%'/>
-          {/* </div> */}
+          </div>;
+        } else if (this.state.currentMailView === '5') {
+          mailView =  <div style={{width: '80%'}}>
+            <div className={style.classes_table_outer_container}>
+              <Switch
+                leftTitle={'По классам'}
+                rightTitle={'По ученикам'}
+                switchView={this.switchView}
+                width='60%'/>
 
-          <div className={style.classes_table_inner_upper_container}>
+                <div className={style.classes_table_inner_upper_container}>
 
-            <div className={style.left_side}>
-              <div className={style.left_title}>
-                Выберите класс/Несколько классов
-              </div>
-              <div className={style.left_grid}>
-                <ClassesGrid classes={this.state.classes} />
-              </div>
-            </div>
-
-            <div className={style.right_side}>
-              <div className={style.right_title}>
-                Текст
-              </div>
-              <textarea
-                className={style.homework_text_area}
-                placeholder='Ваш текст..'
-                maxLength={1000}
-                >
-              </textarea>
-            </div>
-
-          </div>
-
-          <div className={style.classes_table_inner_lower_container}>
-
-            <div className={style.choose}>
-              Выберите дату
-              <div className={style.dd_mm}>
-                ДД/ММ
-              </div>
-              <Checkbox
-                style={{width: '185px'}}
-                label={'Сегодняшнее число'}
-                value={'Сегодняшнее число'}
-                iconStyle={{fill: '#67428F'}}
-                labelStyle={{fontSize: '10px'}}
-              />
-
-            </div>
-            <div className={style.button_send}>
-              <span>
-                Отправить
-
-              </span>
-            </div>
-          </div>
-
-        </div>
-      </div>;
-    }
-
-    return (
-      <MuiThemeProvider>
-        <div className={style.messages_main_container}>
-
-          <div className={style.message_inner_container}>
-
-            <div className={style.messages_title_search_container}>
-
-              <div className={style.messages_title_container}>Личные сообщения</div>
-
-              <div className={style.search_outer_container}>
-
-                <div className={style.search_inner_container}>
-
-                  <div className={style.search_icon}>
-                    <SearchIcon />
+                  <div className={style.left_side}>
+                    {classesDropDownEl}
+                    <div className={style.left_title}>
+                      {leftGridTitle}
+                    </div>
+                    <div className={style.left_grid}>
+                      {gridElement}
+                    </div>
                   </div>
 
-                  <input className={style.search_input}>
+                  <div className={style.right_side}>
+                    <div className={style.right_title}>
+                      Текст
+                    </div>
+                    <textarea
+                      className={style.homework_text_area}
+                      placeholder='Ваш текст..'
+                      maxLength={1000}
+                      >
+                      </textarea>
+                    </div>
 
-                  </input>
+                  </div>
 
-                  <div
-                    className={style.submit_button}>
-                    Поиск
+                  <div className={style.classes_table_inner_lower_container}>
+
+                    {bottomElement}
+                    <div className={style.button_send}>
+                      <span>
+                        Отправить
+
+                      </span>
+                    </div>
                   </div>
 
                 </div>
+              </div>;
+            }
 
-              </div>
+            return (
+              <MuiThemeProvider>
+                <div className={style.messages_main_container}>
 
-            </div>
+                  <div className={style.message_inner_container}>
 
-            <div className={style.messages_body_container}>
-              <MessagesMenu unread={1} important={2} user={'teacher'} changeMailView={this.changeMailView}/>
+                    <div className={style.messages_title_search_container}>
 
-              {mailView}
+                      <div className={style.messages_title_container}>Личные сообщения</div>
 
-            </div>
+                      <div className={style.search_outer_container}>
 
-          </div>
+                        <div className={style.search_inner_container}>
 
-          <Dialog
-            modal={false}
-            open={this.state.open}
-            onRequestClose={this.handleCloseDialog}
-          >
-            <div className={style.popup_container}>
-              <div className={style.to_title}>
-                Кому
-              </div>
-              <input className={style.to_input}>
+                          <div className={style.search_icon}>
+                            <SearchIcon />
+                          </div>
 
-              </input>
-              <div className={style.text_title}>
-                Текст
-              </div>
-              <textarea
-                className={style.text_area}
-                maxLength={500}
-                rows={6}
-                cols={40}>
-              </textarea>
-              <div className={style.send_button}>
-                Отправить
-              </div>
-            </div>
-        </Dialog>
+                          <input className={style.search_input}>
 
-        </div>
-      </MuiThemeProvider>
-    )
-  }
-}
+                          </input>
+
+                          <div
+                            className={style.submit_button}>
+                            Поиск
+                          </div>
+
+                        </div>
+
+                      </div>
+
+                    </div>
+
+                    <div className={style.messages_body_container}>
+                      <MessagesMenu unread={1} important={2} user={'teacher'} changeMailView={this.changeMailView}/>
+
+                      {mailView}
+
+                    </div>
+
+                  </div>
+
+                  <Dialog
+                    modal={false}
+                    open={this.state.open}
+                    onRequestClose={this.handleCloseDialog}
+                    >
+                      <div className={style.popup_container}>
+                        <div className={style.to_title}>
+                          Кому
+                        </div>
+                        <input className={style.to_input}>
+
+                        </input>
+                        <div className={style.text_title}>
+                          Текст
+                        </div>
+                        <textarea
+                          className={style.text_area}
+                          maxLength={500}
+                          rows={6}
+                          cols={40}>
+                        </textarea>
+                        <div className={style.send_button}>
+                          Отправить
+                        </div>
+                      </div>
+                    </Dialog>
+
+                  </div>
+                </MuiThemeProvider>
+              )
+            }
+          }
